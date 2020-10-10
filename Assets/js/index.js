fb9f02becf70249cf4cc4eeb288267062140a85c
Rafael_Estrella <rafaelalfonso82@gmail.com>
2020-10-10 00:20:23 -0400

    klk3

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,13 +1,22 @@
 let filter = new Set();
 let jobs = new Set();
+let filteredJobs = new Array();
+let row = '';
+
 $(async () => {
 
     try {
         const responseJson = await fetch('Assets/json/data.json');
         jobs = await responseJson.json();
+    } catch (error) {
+        console.log(error)
+    }
 
-        let row = '';
+    showJobs();
+});
 
+function showJobs(){
+    if (filteredJobs.length == 0) {
         jobs.forEach(job => {
 
             row += `<div id="tab" class="${job.featured ? 'bd-callout ' : ''} row my-3 shadow rounded-lg filtered">
@@ -64,13 +73,14 @@ $(async () => {
         });
         
         $('#main').append(row);
-
-    } catch (error) {
-        console.log(error)
-    }
     
+    } else {
 
-});
+        // $('#main').html('');
+
+        filteringJobs(filteredJobs);
+    }
+}
 
 function clearBar(){
 
@@ -89,16 +99,15 @@ function addingToArray(tagAdded){
         
         filter.add(tagAdded);
 
-        // TESTING
-        debugger
-        for (let i = 0; i < jobs.size; i++) {
-            if (jobs[i].role == tagAdded) {
-                document.querySelectorAll('.filtered').forEach(t => {
-                    console.log(t)
-                });
-            }
-            
-        }
+        filteredJobs = jobs.filter(job => job.role.toLowerCase() == tagAdded.toLowerCase() 
+        || job.level.toLowerCase() == tagAdded.toLowerCase());
+
+        showJobs();
+
+        filteredJobs.forEach(j => {
+            console.log(j)
+        })
+        
         
         $('#filterLang').removeClass('d-none').addClass('d-flex d-block');
         
@@ -107,6 +116,70 @@ function addingToArray(tagAdded){
     for(let f of filter){
         $('#btn-tags').append(`<button onclick="deletingFilter('${f}')" class="tag clear mx-1 btn btn-xs text-muted mr-auto">${f}</button>`);
     }
+}
+
+function filteringJobs(filteredJobs){
+    $('#main').html('');
+    
+    row = '';
+
+    filteredJobs.forEach(job => {
+
+        row += `<div id="tab" class="${job.featured ? 'bd-callout ' : ''} row my-3 shadow rounded-lg filtered">
+        <div class="mr-3">
+            <img " class="ml-4 my-4" src="${'Assets/' + job.logo}">
+        </div>
+        
+        <div style="margin-top: 26px;">
+                <label class="text-muted  company">${job.company}</label>
+                ${job.new ? `<label class="rounded-pill text-white lbl-new">NEW!</label>`:``}
+                ${job.featured ? `<label class="rounded-pill text-white lbl-featured">FEATURED</label>` : ``}
+                <p class="font-weight-bold title mb-0">${job.position}</p>
+                <label class="text-muted company">
+                    ${job.postedAt} <span class="mx-2">.</span> 
+                    ${job.contract} <span class="mx-2">.</span>
+                    ${job.location}
+                </label>
+        </div>
+        
+        <div class="d-inline my-auto ml-auto mr-5" id="filters">
+                
+                <button id="btn-role" 
+                data-value="${job.role}" 
+                type="button" 
+                class="mx-1 btn btn-xs text-muted test"
+                onclick="addingToArray('${job.role}')">
+                    ${job.role}
+                </button>
+
+                <button id="btn-level" 
+                data-value="${job.level}" 
+                type="button" 
+                class="mx-1 btn btn-xs text-muted test"
+                onclick="addingToArray('${job.level}')">
+                    ${job.level}
+                </button>
+
+                ${
+                    job.languages.map( lang => {
+                        return `<button 
+                        id="btn-languages" 
+                        data-value="${lang}"  
+                        type="button" 
+                        class="mx-1 btn btn-xs text-muted test"
+                        onclick="addingToArray('${lang}')">
+                             ${lang} 
+                        </button>`
+                    }).join('')
+                }
+                
+            </div>
+            
+        </div>`;
+    });
+
+    $('#main').append(row);
+
 }
 
 function deletingFilter(tagToDelete){
@@ -124,8 +197,4 @@ function deletingFilter(tagToDelete){
 
     }
     
-}
-
-function filteringJobs(){
-
 }
